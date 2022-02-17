f3724324f5f43e0dd51635559e12bc4a60eb3d30
Michael Buffone <m_buffone@hotmail.com>
2022-02-17 09:25:45 -0500

    finished the survey

--- survey.js
+++ survey.js
@@ -5,8 +5,28 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+let profile = [];
 
-  rl.close();
+rl.question(`What's your name? Nicknames are also acceptable: `, (answer) => {
+  profile.push(`${answer}`);
+  rl.question(`What's an activity you like doing? `, (answer) => {
+    profile.push(`likes  ${answer}`);
+    rl.question('What do you listen to while doing that? ', (answer) => {
+      profile.push(`while listening to ${answer}.`);
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)?', (answer) => {
+        profile.push(`Favorite meal time is ${answer},`);
+        rl.question(`What's your favourite thing to eat for that meal?`, (answer) => {
+          profile.push(`their favorite part of that meal is ${answer}.`);
+          rl.question('Which sport is your absolute favourite?', (answer) => {
+            profile.push(`Thier favorite sport is ${answer},`);
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer) => {
+              profile.push(`and their superpower is ${answer}`);
+              console.log(profile.join(" "));
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
